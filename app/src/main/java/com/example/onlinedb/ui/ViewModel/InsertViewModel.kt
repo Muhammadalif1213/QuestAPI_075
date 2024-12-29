3794939a65c4149b050741dcd7b6d5c837ceb0c8
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-29 15:11:18 +0700

    Menambahkan data class InsertUiEvent untuk input data mahasiswa.

---
+++ app/src/main/java/com/example/onlinedb/ui/ViewModel/InsertViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.onlinedb.ui.ViewModel
+
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
