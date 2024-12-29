f81131e2dd1d01c223c71f527b8a103019b7a0f1
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-30 00:41:39 +0700

    Menambahkan alamat dan fungsi HomeScreen untuk tampilan daftar mahasiswa, tombol tambah mahasiswa, dan opsi refresh serta hapus data.

--- app/src/main/java/com/example/onlinedb/ui/View/HomeView.kt
+++ app/src/main/java/com/example/onlinedb/ui/View/HomeView.kt
@@ -15,23 +15,92 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.onlinedb.R
 import com.example.onlinedb.model.Mahasiswa
+import com.example.onlinedb.ui.CustomWidget.CustomTopAppBar
+import com.example.onlinedb.ui.Navigation.DestinasiNavigasi
 import com.example.onlinedb.ui.ViewModel.HomeUiState
+import com.example.onlinedb.ui.ViewModel.HomeViewModel
+import com.example.onlinedb.ui.ViewModel.PenyediaViewModel
+
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes= "Home Mhs"
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+ val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+ Scaffold (
+     modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+     topBar = {
+         CustomTopAppBar(
+             title = DestinasiHome.titleRes,
+             canNavigateBack = false,
+             scrollBehavior = scrollBehavior,
+             onRefresh = {
+                 viewModel.getMhs()
+             }
+
+         )
+     },
+     floatingActionButton = {
+         FloatingActionButton(
+             onClick = navigateToItemEntry,
+             shape = MaterialTheme.shapes.medium,
+             modifier = Modifier.padding(18.dp)
+         ){
+             Icon(
+                 imageVector = Icons.Default.Add,
+                 contentDescription = "Add Mahasiswa"
+             )
+         }
+     }
+ ){
+     innerPadding ->
+     HomeStatus(
+         homeUiState = viewModel.mhsUiState,
+         retryAction = { viewModel.getMhs() },
+         modifier = Modifier
+             .padding(innerPadding),
+         onDetailClick = onDetailClick,
+         onDeleteClick = {
+             viewModel.deleteMhs(it.nim)
+             viewModel.getMhs()
+         }
+     )
+ }
+}
+
 
 @Composable
 fun HomeStatus(
