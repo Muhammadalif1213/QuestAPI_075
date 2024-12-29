55fc8e494359fcd03921dce3da8896bb2ef1747f
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-29 16:53:27 +0700

    Membuat fungsi menampilkan halaman inputan data mahasiswa dan memberikan alamat

--- app/src/main/java/com/example/onlinedb/ui/View/InsertView.kt
+++ app/src/main/java/com/example/onlinedb/ui/View/InsertView.kt
@@ -4,23 +4,76 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.onlinedb.ui.CustomWidget.CustomTopAppBar
 import com.example.onlinedb.ui.Navigation.DestinasiNavigasi
 import com.example.onlinedb.ui.ViewModel.InsertUiEvent
+import com.example.onlinedb.ui.ViewModel.InsertUiState
+import com.example.onlinedb.ui.ViewModel.InsertViewModel
+import kotlinx.coroutines.launch
 
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes= "Entry Mhs"
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+        innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
 
 @Composable
 fun EntryBody(
-    insertUiState: InsertUiEvent,
+    insertUiState: InsertUiState,
     onSiswaValueChange: (InsertUiEvent) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
@@ -30,7 +83,7 @@ fun EntryBody(
       modifier = modifier.padding(12.dp)
   )  {
       FormInput(
-          insertUiEvent = insertUiState,
+          insertUiEvent = insertUiState.insertUiEvent,
           onValueChange = onSiswaValueChange,
           modifier = Modifier.fillMaxWidth()
       )
