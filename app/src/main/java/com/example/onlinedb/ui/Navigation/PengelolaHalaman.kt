84878d1952a0964def5da78f477fcc46687f5a96
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-30 01:21:17 +0700

    Menambahkan navigasi untuk halaman home dan entry

--- app/src/main/java/com/example/onlinedb/ui/Navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/onlinedb/ui/Navigation/PengelolaHalaman.kt
@@ -19,5 +19,23 @@ fun PengelolaHalaman(
         startDestination = DestinasiHome.route,
         modifier = Modifier)
     {
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                onDetailClick = {
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive= true
+                    }
+                }
+            })
+        }
     }
 }
