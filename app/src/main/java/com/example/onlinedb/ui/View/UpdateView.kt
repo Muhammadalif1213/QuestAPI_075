752a5a4ea3e4eb356d7f39a562c6c11d97f07507
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-12-30 12:35:07 +0700

    Membuat fungsi UpdateView untuk mengedit data mahasiswa dengan menyambungkan inputan isert ke UpdateViewModel.

---
+++ app/src/main/java/com/example/onlinedb/ui/View/UpdateView.kt
@@ -0,0 +1,46 @@
+package com.example.onlinedb.ui.View
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.onlinedb.ui.CustomWidget.CustomTopAppBar
+import com.example.onlinedb.ui.ViewModel.PenyediaViewModel
+import com.example.onlinedb.ui.ViewModel.UpdateViewModel
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateView(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    updateViewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory))
+{
+        val coroutineScope = rememberCoroutineScope()
+        Scaffold(
+            topBar = {
+                CustomTopAppBar(
+                    title = "",
+                    canNavigateBack = true,
+                    navigateUp = navigateBack
+                )
+            },
+            modifier = modifier
+        ) {innerPadding ->
+            EntryBody(
+                insertUiState = updateViewModel.updateUiState,
+                onSiswaValueChange = updateViewModel::updateMahasiswaState,
+                onSaveClick = {
+                    coroutineScope.launch {
+                        updateViewModel.updateMahasiswa()
+                        navigateBack()
+                    }
+                },
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
